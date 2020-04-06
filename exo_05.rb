c515f9bc8b83de7df8cacc744acfc81bd9682ad1
Snakdoc <mehdi_bezghiche@yahoo.fr>
2020-04-06 17:24:18 -0400

    Ajout Exo 5

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" #Affiche 'On va compter le nombre d'heures de travail à THP'
+puts "Travail : #{10 * 5 * 11}" # Calcul de l'opération avec affichage de celle ci
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # Pareille
+
+puts "Et en secondes ?" # Affiche 'Et en secondes?'
+
+puts 10 * 5 * 11 * 60 * 60 # Fais l'opération et l'affice
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #Affiche "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7 #Fais l'opération
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"  # Affiche "Ça fait combien 3 + 2 ?" et fais l'opération tout en l'affichant
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"  # Affiche "Ça fait combien 5 - 7 ?" et fais l'opération tout en l'affichant
+
+puts "Ok, c'est faux alors !" #Affiche "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :" #Affiche "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # Affiche "Est-ce que 5 est plus grand que -2 ? avec fulse ou true en fonciton de la réponse
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"  # Affiche "Est-ce que 5 est supérieur ou égal à -2 ?" avec fulse ou true en fonciton de la réponse
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"  # Affiche "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" avec false ou true en fonction de la réponse
