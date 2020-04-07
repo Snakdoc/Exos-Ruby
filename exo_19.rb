cbdacdbe64d7f17387035a5f4ba32463ba39ad30
Snakdoc <mehdi_bezghiche@yahoo.fr>
2020-04-07 14:11:51 -0400

    Fakossa Tips

--- exo_19.rb
+++ exo_19.rb
@@ -1,32 +1,9 @@
-list_email = []
-50.times do |i|
-  number = i +1
-  email = "jean.dupont.#{"%02d"%number}@email.fr"
-  #puts email
-  list_email << email
+number = 1
+50.times do
+  email = "%s.%s%#02d@email.fr" % ["jean","dupont", number]
+    number = number +1
+    if number %2 != 0
+      puts email
+    else
+    end
 end
-puts list_email [1]
-puts list_email [3]
-puts list_email [5]
-puts list_email [7]
-puts list_email [9]
-puts list_email [11]
-puts list_email [13]
-puts list_email [15]
-puts list_email [17]
-puts list_email [19]
-puts list_email [21]
-puts list_email [23]
-puts list_email [25]
-puts list_email [27]
-puts list_email [29]
-puts list_email [31]
-puts list_email [33]
-puts list_email [35]
-puts list_email [37]
-puts list_email [39]
-puts list_email [41]
-puts list_email [43]
-puts list_email [45]
-puts list_email [47]
-puts list_email [49]
